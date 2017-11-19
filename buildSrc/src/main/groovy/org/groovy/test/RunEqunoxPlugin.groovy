9ed9b2b312bb7f3c3835b0163930d35eb00c4a7b
Mladen Kralev <mladenkralev@gmail.com>
2017-11-19 23:14:40 +0200

    Adding method for creating bat script file and refactor code

--- buildSrc/src/main/groovy/org/groovy/test/RunEqunoxPlugin.groovy
+++ buildSrc/src/main/groovy/org/groovy/test/RunEqunoxPlugin.groovy
@@ -4,12 +4,10 @@ import groovy.io.FileType
 import groovy.util.logging.Slf4j
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.logging.Logger
 
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
-
 
 /**
  * Created by mladen on 11/18/2017.
@@ -26,22 +24,29 @@ class RunEqunoxPlugin implements Plugin<Project> {
         project.task('startContainer') {
             doLast {
                 List<File> listOfFilesInBinaryFolder  = getFilesInBinaryFolder(project)
+
                 createContainerFolder(project)
+
                 copyToBuildDir(listOfFilesInBinaryFolder, project)
+
+                createBatScript(project)
             }
         }.dependsOn("build")
     }
 
     List<File> getFilesInBinaryFolder(Project project) {
-        String binaryFolderName = 'binary'
+        Path containerFolderPath = Paths.get("" +
+                "${project.rootDir}" +
+                "${File.separator}binary")
 
-        File binaryFolder = new File("${project.rootDir}${File.separator}${binaryFolderName}")
+        File binaryFolder = new File(containerFolderPath.toString())
+
         log.info("Binary folder found: $binaryFolder ")
         assert binaryFolder.exists() , "Binary folder doesn't exist!"
 
-        def listOfFilesInBinaryFolder = []
-
         log.info("Binary folder files:")
+
+        def listOfFilesInBinaryFolder = []
         binaryFolder.eachFileRecurse(FileType.FILES) { file ->
             listOfFilesInBinaryFolder << file
             log.info("Added in list: $file ")
@@ -54,30 +59,52 @@ class RunEqunoxPlugin implements Plugin<Project> {
     }
 
     boolean copyToBuildDir(List<File> listOfFilesInBinaryFolder, Project project) {
-        String destinationAsString = "${project.rootDir}" +
-                                     "${File.separator}build" +
-                                     "${File.separator}libs" +
-                                     "${File.separator}container"
-
-        Path destination = Paths.get(destinationAsString)
+        Path containerFolderPath = Paths.get("${project.rootDir}" +
+                "${File.separator}build" +
+                "${File.separator}libs" +
+                "${File.separator}container")
 
         listOfFilesInBinaryFolder.each {file ->
-            Files.copy(file.toPath(), destination.resolve(file.getName()))
-            log.info("File copied from $file.path to $destination")
+            Files.copy(file.toPath(), containerFolderPath.resolve(file.getName()))
+            log.info("File copied from $file.path to $containerFolderPath")
         }
     }
 
     boolean createContainerFolder(Project project) {
-        String folderDestinationAsString = "${project.rootDir}" +
+        Path containerFolderPath = Paths.get("${project.rootDir}" +
                 "${File.separator}build" +
                 "${File.separator}libs" +
-                "${File.separator}container"
-        File destinationFolder = new File(folderDestinationAsString)
+                "${File.separator}container")
+
+        File destinationFolder = new File(containerFolderPath.toString())
 
         assert !destinationFolder.exists() , "Destination folder is already created!"
 
         destinationFolder.mkdir()
 
         assert destinationFolder.exists() , "Destination folder wasn't created!"
+    }
+
+    boolean createBatScript(Project project) {
+        Path folderRuntimePath = Paths.get(
+                "${project.rootDir}" +
+                "${File.separator}build" +
+                "${File.separator}runtime")
+
+        File directoryBatFile = new File(folderRuntimePath.toString())
+        assert !directoryBatFile.exists() , "$directoryBatFile already exists!"
+
+        directoryBatFile.mkdir()
+        assert directoryBatFile.exists() , "$directoryBatFile dir wasn't created!"
+
+        Path batFilePath = Paths.get(
+                "${project.rootDir}" +
+                "${File.separator}build" +
+                "${File.separator}runtime" +
+                "${File.separator}start.bat")
+
+        File batFile = new File(batFilePath.toString())
+        batFile.createNewFile()
+        assert batFile.exists() , "$batFile wasn't created!"
     }
 }
